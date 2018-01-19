b0b67e46b57725bfbbdf425128a0004b2dd18723
Daniel Bellonzi <bellonzi@Daniels-MacBook-Air.local>
2018-01-18 23:21:39 -0800

    Added Timer

--- runster/ViewController.swift
+++ runster/ViewController.swift
@@ -11,45 +11,55 @@ import CoreData
 import CoreLocation
 import MapKit
 
-class ViewController: UIViewController, CLLocationManagerDelegate {
+class ViewController: UIViewController, CLLocationManagerDelegate, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var splitTable: UITableView!
-
     @IBOutlet weak var currentTime: UILabel!
     @IBOutlet weak var currentDistance: UILabel!
-    var run = [RunDistance]()
-    var totalDistance: Double = 0.0
-    var totalsplitDistance: Double = 0.0
+    
+    //Stopwatch Value
     var isRunner: Bool = false
-//    var lastMarkedLocation = [String: Double]()
-//    var lastRunLocation = [String: Double]()
+    
+    //Distance
+    var totalDistance: Double = 0.0
+    var splitDistance: Double = 0.0
+
+    //Timer
+    var timer: Timer?
+    var time: Int = 0
+    var splitTime: Int = 0
+    
+    //Split Values
+    var splitTimes = [Int]()
+    var splitDist = [Double]()
+    
+    //Locations
     var currentLocation: CLLocation?
     var location: CLLocation?
-    
     let locationManager = CLLocationManager()
-    let managedObjectContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
+    //init
     override func viewDidLoad() {
         super.viewDidLoad()
+        splitTable.dataSource = self
+        splitTable.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
         enableBasicLocationServices()
+        loadViewData()
     }
     
     
     func enableBasicLocationServices() {
         locationManager.delegate = self
-        
         switch CLLocationManager.authorizationStatus() {
         case .notDetermined:
             // Request when-in-use authorization initially
             locationManager.requestWhenInUseAuthorization()
             break
-            
         case .restricted, .denied:
             // Disable location features
             disableMyLocationBasedFeatures()
             break
-            
         case .authorizedWhenInUse, .authorizedAlways:
             // Enable location features
             enableMyWhenInUseFeatures()
@@ -63,11 +73,9 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         case .restricted, .denied:
             disableMyLocationBasedFeatures()
             break
-            
         case .authorizedWhenInUse:
             enableMyWhenInUseFeatures()
             break
-            
         case .notDetermined, .authorizedAlways:
             break
         }
@@ -84,44 +92,77 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         locationManager.startUpdatingLocation()
     }
     
-    func openRun() {
-        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "RunDistance")
-        do {
-            let result = try managedObjectContext.fetch(request)
-            run = result as! [RunDistance]
-        } catch {
-            print("\(error)")
-        }
-    }
-    
-    //call location
+    //Update Location
     func locationManager(_ manager: CLLocationManager,  didUpdateLocations locations: [CLLocation]) {
         currentLocation = locations.last!
         if isRunner {
             self.totalDistance += (currentLocation?.distance(from: location!))!
-            currentDistance.text = String(format: "%.2f", totalDistance)
-            self.totalsplitDistance += (currentLocation?.distance(from: location!))!
-            splitDistance.text = String(format: "%.2f", totalsplitDistance)
+            self.splitDistance += (currentLocation?.distance(from: location!))!
         }
         location = currentLocation
     }
     
+    //Start Button
     @IBAction func markLocation(_ sender: UIButton) {
         if isRunner {
             isRunner = false
             sender.setTitle("Start", for: UIControlState.normal)
+            timer?.invalidate()
         } else {
-            totalDistance = 0.0
-            totalsplitDistance = 0.0
             location = currentLocation
             isRunner = true
             sender.setTitle("Stop", for: UIControlState.normal)
+            timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true, block: { _ in
+                self.time += 1
+                self.splitTime += 1
+                self.loadViewData()
+            })
         }
     }
+    
+    //Split Button
     @IBAction func splitTimeButton(_ sender: Any) {
-        totalsplitDistance = 0.0
+        splitDist.insert(splitDistance, at: 0)
+        splitTimes.insert(splitTime, at: 0)
+        splitDistance = 0.0
+        splitTime = 0
+        loadViewData()
+        loadTable()
     }
     
+    //Reset Button
     @IBAction func resetButton(_ sender: UIButton) {
+        if isRunner {
+            isRunner = false
+            sender.setTitle("Start", for: UIControlState.normal)
+            timer?.invalidate()
+        }
+        totalDistance = 0.0
+        time = 0
+        splitDist = []
+        splitTimes = []
+        loadTable()
+        loadViewData()
     }
-}
+    
+    //Load View
+    func loadViewData() {
+        currentTime.text = String(self.time)
+        currentDistance.text = String(self.totalDistance)
+    }
+    
+    func loadTable() {
+        splitTable.reloadData()
+    }
+    
+    //Table Functions
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return splitDist.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath)
+        cell.textLabel?.text = String(splitTimes[indexPath.row])
+        cell.detailTextLabel?.text = String(splitDist[indexPath.row])
+        return cell
+    }}
